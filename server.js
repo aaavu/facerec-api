645b9d75548abac4894094ddd65f3b90e81dd71b
Anthony Vu <git@anthonyvu.dev>
2019-08-30 10:56:01 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const app = express();
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '',
+    database: 'smartbrain'
+  }
+})
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {res.send('hello')});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt)); 
+app.get('/profile/:id', profile.handleProfile(db));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', image.handleApiCall());
+
+app.listen('3000', () => {
+  console.log('app is running on port 3000');
+});
